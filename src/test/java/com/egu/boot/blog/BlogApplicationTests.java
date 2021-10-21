78082334669579bcc2b12e43f00f6d4b715960fd
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2021-10-21 14:29:43 +0900

    환경 셋팅 완료

---
+++ src/test/java/com/egu/boot/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egu.boot.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
